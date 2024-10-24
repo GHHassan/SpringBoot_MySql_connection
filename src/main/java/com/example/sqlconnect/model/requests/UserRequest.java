119480b5a9046d3c58c58e439c7cc642107808ae
Hassan <66774893+GHHassan@users.noreply.github.com>
2024-10-24 19:50:11 +0100

    User class was unused and turned to a parent class

--- src/main/java/com/example/sqlconnect/model/requests/UserRequest.java
+++ src/main/java/com/example/sqlconnect/model/requests/UserRequest.java
@@ -1,43 +1,10 @@
 package com.example.sqlconnect.model.requests;
 
-public class UserRequest {
-    private String  firstName;
-    private String lastName;
-    private String  email;
-    private String password;
+import com.example.sqlconnect.model.User;
+
+public class UserRequest extends User {
+
     private String userId = null;
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 
     public String getUserId() {
         return userId;
--- src/main/java/com/example/sqlconnect/shared/dto/UserDto.java
+++ src/main/java/com/example/sqlconnect/shared/dto/UserDto.java
@@ -1,18 +1,16 @@
 package com.example.sqlconnect.shared.dto;
 
+import com.example.sqlconnect.model.User;
+
 import java.io.Serial;
 import java.io.Serializable;
 
-public class UserDto implements Serializable {
+public class UserDto extends User implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
     private long id;
     private String userId;
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String password;
     private String encryptedPassword;
     private String emailVerification;
     private Boolean emailVerificationStatus = false;
@@ -31,38 +29,6 @@ public class UserDto implements Serializable {
 
     public void setUserId(String userId) {
         this.userId = userId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public String getEncryptedPassword() {
